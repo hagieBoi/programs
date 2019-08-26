bbd6e2f17a0109aa15876c81e1d6e129ee985a33
Seth Hager <shager@lappy21.cs.nmsu.edu>
2019-08-26 14:08:24 -0600

    Add existing file

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+//Author: Seth Hager
+//Date: August 26th, 2019
+
+//Lab 1
+
+
+package helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+		return;
+		
+	} 
+}
