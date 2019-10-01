c755c543fb67c405d2fcaa0f174a0994c91353f0
Seth Hager <shager@nmsu.edu>
2019-09-30 23:57:39 -0600

    Program 2

--- simpleWebServer/src/WebWorker.java
+++ simpleWebServer/src/WebWorker.java
@@ -3,12 +3,12 @@
 * to receive and respond to a single HTTP request. After the constructor
 * the object executes on its "run" method, and leaves when it is done.
 *
-* One WebWorker object is only responsible for one client connection. 
+* One WebWorker object is only responsible for one client connection.
 * This code uses Java threads to parallelize the handling of clients:
 * each WebWorker runs in its own thread. This means that you can essentially
-* just think about what is happening on one client at a time, ignoring 
+* just think about what is happening on one client at a time, ignoring
 * the fact that the entirety of the webserver execution might be handling
-* other clients, too. 
+* other clients, too.
 *
 * This WebWorker class (i.e., an object of this class) is where all the
 * client interaction is done. The "run()" method is the beginning -- think
@@ -16,13 +16,13 @@
 * a row, invoking three methods in this class: it reads the incoming HTTP
 * request; it writes out an HTTP header to begin its response, and then it
 * writes out some HTML content for the response content. HTTP requests and
-* responses are just lines of text (in a very particular format). 
+* responses are just lines of text (in a very particular format).
 *
 * Author: Seth Hager
 * Last Modified: September 19th, 2019
 *
 * Modifications made: Made it so the if a file path is requested the programs serves the file requested
-* If the file doesn't exist then is shows and ERROR 404 page. 
+* If the file doesn't exist then is shows and ERROR 404 page.
 *
 **/
 
@@ -38,7 +38,8 @@ import java.util.Arrays;
 public class WebWorker implements Runnable
 {
 private static File file;
-    
+private static String contentType = "";
+private static String filepath = "";
 private Socket socket;
 
 /**
@@ -50,7 +51,7 @@ public WebWorker(Socket s)
 }
 
 /**
-* Worker thread starting point. Each worker handles just one HTTP 
+* Worker thread starting point. Each worker handles just one HTTP
 * request and then returns, which destroys the thread. This method
 * assumes that whoever created the worker created it with a valid
 * open socket object.
@@ -62,7 +63,7 @@ public void run()
       InputStream  is = socket.getInputStream();
       OutputStream os = socket.getOutputStream();
       readHTTPRequest(is);
-      writeHTTPHeader(os,"text/html");
+      writeHTTPHeader(os,contentType);
       writeContent(os);
       os.flush();
       socket.close();
@@ -79,20 +80,14 @@ public void run()
 private void readHTTPRequest(InputStream is)
 {
    String line;
-   String path;
    BufferedReader r = new BufferedReader(new InputStreamReader(is));
    while (true) {
       try {
-         while (!r.ready()) Thread.sleep(1);
-         line = r.readLine();
-         if(line.contains("GET")){						// Finds GET request and strips it of the requested file path
-            path = line.substring(5, line.length()-1);
-            path = path.substring(0,path.indexOf(' '));
-			file = new File(path);
-            // System.err.println(file.exists());	Debug statement
-            }
-         System.err.println("Request line: ("+line+")");
-         if (line.length()==0) break;
+        while (!r.ready()) Thread.sleep(1);
+        line = r.readLine();
+        System.err.println("Request line: (" + line + ")");
+        if (line.length()==0) break;
+        if(line.contains("GET"))handleGet(line);
       } catch (Exception e) {
          System.err.println("Request error: "+e);
          break;
@@ -108,10 +103,11 @@ private void readHTTPRequest(InputStream is)
 **/
 private void writeHTTPHeader(OutputStream os, String contentType) throws Exception
 {
+   System.err.println("\n" + filepath + " " + contentType +"\n");
+   file = new File(filepath);
    Date d = new Date();
    DateFormat df = DateFormat.getDateTimeInstance();
    df.setTimeZone(TimeZone.getTimeZone("GMT"));
-   
    if(file.exists()) os.write("HTTP/1.1 200 OK\n".getBytes());
    else {os.write("HTTP/1.1 404 OK\n".getBytes());} //If file path doesn't exist then it sets the HTML code to 404
    os.write("Date: ".getBytes());
@@ -119,7 +115,7 @@ private void writeHTTPHeader(OutputStream os, String contentType) throws Excepti
    os.write("\n".getBytes());
    os.write("Server: Jon's very own server\n".getBytes());
    //os.write("Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\n".getBytes());
-   //os.write("Content-Length: 438\n".getBytes()); 
+   //os.write("Content-Length: 438\n".getBytes());
    os.write("Connection: close\n".getBytes());
    os.write("Content-Type: ".getBytes());
    os.write(contentType.getBytes());
@@ -144,20 +140,28 @@ private void writeContent(OutputStream os) throws Exception
    os.write("</body></html>\n".getBytes());
    }
 }
-public static byte[] fileToByte(File f) throws Exception		
+public static byte[] fileToByte(File f) throws Exception
 //OutputStreams only take binary and the file doesn't have a getByte function like String so I have to create my own.
 {
-	//Date date = new Date();    
-	FileInputStream fis = null;    	
-	byte[] bStream = new byte[(int) f.length()];
+	  FileInputStream fis = null;
+	  byte[] bStream = new byte[(int) f.length()];
     fis = new FileInputStream(file);
     fis.read(bStream);
     fis.close();
-	//String s = new String(bStream);				//FIXME getting a weird encoding on print statement
-    //System.out.println(s);
-	//s.replace("<cs371date>", ("<h2> " + date.toString() + " </h2>"));
-	//s.replace("<cs371server>", "<h2>Welcome to my server!</h2>");
-	//bStream = s.getBytes();
+
     return bStream;
 }
+public static void handleGet(String request){
+  String[] path = request.split(" ");
+  filepath = path[1].substring(1,path[1].length());
+  if(filepath.contains("gif")){contentType = "image/gif";}
+  if(filepath.contains("jpeg")){contentType = "image/jpeg";}
+  if(filepath.contains("png")){contentType = "image/png";}
+  if(filepath.contains("html")){contentType = "text/html";}
+  if(filepath.contains("css")){contentType = "text/css";}
+
+
+}
+
+
 } // end class
